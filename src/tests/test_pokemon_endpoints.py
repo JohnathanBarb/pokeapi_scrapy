9febf706948e5f71e726757d6cf6a2b3aa3e6232
johnathan barbosa <jbarbosa0504@gmail.com>
2022-03-01 22:06:44 -0300

    test: added tests for post_pokemon endpoint

--- src/tests/test_pokemon_endpoints.py
+++ src/tests/test_pokemon_endpoints.py
@@ -2,9 +2,10 @@ from typing import List
 import pytest, httpx
 
 from fastapi import status
-from src.models.pokemon import PokemonDB
+from src.models.pokemon import PokemonDB, Stat
 
 from src.tests.conftest import test_client, initial_pokemons
+
 
 @pytest.mark.asyncio
 async def test_home(
@@ -35,8 +36,6 @@ class TestGetPokemon:
         json = response.json()
 
         assert json['detail'] == "Not Found"
-        
-        
 
     async def test_existing(
         self,
@@ -50,3 +49,59 @@ class TestGetPokemon:
 
         json = response.json()
         assert json["_id"] == str(initial_pokemons[0].id)
+
+
+@pytest.mark.asyncio
+class TestPostPokemon:
+    async def test_with_missing_data(
+        self,
+        test_client: httpx.AsyncClient,
+    ):
+        data = PokemonDB(
+            name="John1",
+            height=185,
+            weight=100,
+            types=['human'],
+            stats=[
+                Stat(name='attack', base_stat=10),
+                Stat(name='defense', base_stat=10)
+            ],
+            moves=['move', 'code']
+        )
+        response = await test_client.post(
+            '/pokemons/',
+            data='{"name": "poke_test", "height": 10}'
+        )
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+        json = response.json()
+
+        assert json['detail'][0]['msg'] == "field required"
+    
+    async def test_with_correct_data(
+        self,
+        test_client: httpx.AsyncClient,
+    ):
+        data = PokemonDB(
+            name="John1",
+            height=185,
+            weight=100,
+            types=['human'],
+            stats=[
+                Stat(name='attack', base_stat=10),
+                Stat(name='defense', base_stat=10)
+            ],
+            moves=['move', 'code']
+        )
+
+        response = await test_client.post(
+            '/pokemons/',
+            content=data.json()
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        json = response.json()
+
+        assert json['name'] == data.name
